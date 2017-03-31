50688ed9dab3a45e581d3cb8f450275cfe0e3afe
Mark <markjconlon@gmail.com>
2017-03-31 13:55:54 -0400

    fixed arguments error in create and missed an end in hunt method

--- vampire.rb
+++ vampire.rb
@@ -1,18 +1,29 @@
 class Vampire
   @@coven = []
+  @@night = true
 
   def self.create(name, age)
-    vampire = Vampire.new(name, age)
+    vampire = Vampire.new
     @@coven.push(vampire)
     return vampire
   end
 
+  def self.sunrise
+
+
   def initialize(name, age)
     @name = name
     @age = age
-    @in_coffin = true              #starting the vampire in coffin waking up no blood yet
+    @in_coffin = true               #starting the vampire in coffin waking up no blood yet
     @drank_blood_today = false
   end
 
+  def drink_blood
+    @drank_blood_today = true
+  end
 
+  def hunt
+    drink_blood if rand(5) <= 1      #60 % chance they will be able to feed
+    end
+  end
 end
